24a733a73bf10cd5e6c14f0821702146f87a6cab
KsenijaBojchevska <94746177+KsenijaBojchevska@users.noreply.github.com>
2023-04-15 17:31:10 +0200

    Add files via upload

---
+++ homework no. 18 terraform modules/vm_modules/variables.tf
@@ -0,0 +1,28 @@
+variable "base_name" {
+    type = string
+    default = "test"
+  
+}
+variable "location" {
+    type = string
+    default = "West Europe"
+  
+}
+
+variable "my_public_ip" {
+    type = string
+    default = "92.53.12.67"
+  
+}
+
+variable "my_password" {
+    type = string
+    default = "admin55555#"
+  
+}
+
+variable "vms_subnet_id" {
+    type = string
+    default = "10.0.1.0/24"
+  
+} 
